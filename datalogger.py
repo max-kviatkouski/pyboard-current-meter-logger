1f71738f69650776ea1095003f57c88833523613
Max Kviatkouski <kvyatkovskij@gmail.com>
2018-05-14 22:42:52 -0700

    optimized output: put timestamp only to a first record in a series

--- datalogger.py
+++ datalogger.py
@@ -9,11 +9,18 @@ DELAY_BETWEEN_CYCLES = 5000
 def log_series(delay = 20, duration = 5000):
     with open('/sd/log.csv', 'a+') as log:
         n = duration // delay
-        for i in range(1, n+1):
-            t = pyb.millis()
+        t = pyb.millis()
+        x, y, z = accel.filtered_xyz()
+        log.write('{},{},{},{}\n'.format(t, x, y, z))
+        for i in range(2, n+1):
             x, y, z = accel.filtered_xyz()
-            log.write('{},{},{},{}\n'.format(t, x, y, z))
+            log.write(',{},{},{}\n'.format(x, y, z))
 
+def log_file_header():
+    with open('/sd/log.csv', 'a+') as log:
+        log.write('Time,X,Y,Z\n')
+
+log_file_header()
 while True:
     log_series()
     blink_led(red)
