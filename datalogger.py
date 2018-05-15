5f737ec2338f7c5ea9e04dcffa3802fbc7e11b9a
Max Kviatkouski <kvyatkovskij@gmail.com>
2018-05-14 22:32:35 -0700

    added logging of series of measurements; changed deply script to clean pyboard storage first

--- datalogger.py
+++ datalogger.py
@@ -4,17 +4,20 @@ from util import *
 accel = pyb.Accel()
 switch = pyb.Switch()
 
-while True:
-	if switch():
-		pyb.delay(200)
-        log = open('/sd/log.csv', 'w')
+DELAY_BETWEEN_CYCLES = 5000
 
-        while not switch():
-            # t = pyb.millis()
-            # x, y, z = accel.filtered_xyz()
-            # log.write('{},{},{},{}\n'.format(t,x,y,z))
-            blink_led(red)
-            
-        log.close()
+def log_series(delay = 20, duration = 5000):
+    with open('/sd/log.csv', 'a+') as log:
+        n = duration // delay
+        for i in range(1, n+1):
+            t = pyb.millis()
+            x, y, z = accel.filtered_xyz()
+            log.write('{},{},{},{}\n'.format(t, x, y, z))
+
+while True:
+    log_series()
+    blink_led(red)
+    pyb.delay(DELAY_BETWEEN_CYCLES)
+    if switch():
         blink_led(blue)
-        pyb.delay(200)
+        break
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+rm -rfv /media/max/0C4E-1741/*
 cp -v *.py /media/max/0C4E-1741/
