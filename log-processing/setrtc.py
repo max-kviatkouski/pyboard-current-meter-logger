8a6a21d5823238619d2887df26cebb522b060578
Max Kviatkouski <kvyatkovskij@gmail.com>
2018-05-21 19:15:32 -0700

    updated setrtc script to fix a bug where nothing was written to calibration file

---
+++ log-processing/setrtc.py
@@ -0,0 +1,27 @@
+from ampy.pyboard import Pyboard
+from datetime import datetime
+
+def write_to_console(data):
+    print(data.decode('ascii'), end='')
+
+def main():
+    pyb = Pyboard('/dev/ttyACM0')
+    pyb.enter_raw_repl()
+    now = datetime.today()
+    print('Setting time')
+    pyb.exec_raw(
+        "rtc = pyb.RTC()\n" \
+        "t = ({0},{1},{2},{3},{4},{5},{6},{7})\n"
+        "rtc.datetime(t)\n" \
+        "print('Time now is:')\n" \
+        "print(rtc.datetime())\n" \
+        "with open('datetime.correction', 'w+') as f:\n" \
+        "    f.write('Initialized RTC with: ' + str(t))\n"
+        "".format(now.year, now.month, now.day, now.date().weekday() + 1, now.hour, now.minute, now.second + 1, 0),
+        data_consumer=write_to_console
+    )
+    pyb.exit_raw_repl()
+    print('Done!')
+
+if __name__ == "__main__":
+    main()
