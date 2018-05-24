cccd144a9538b45c19f429305ffa450b39f80ac2
Max Kviatkouski <kvyatkovskij@gmail.com>
2018-05-23 18:44:12 -0700

    switched from using pyb.standby() to pyb.stop() - makes code easier to understand and timings more precise; added daily log rotation; refactored settings to make them easy to change constants in the beginning of datalogging file

--- logger-firmware/datalogger.py
+++ logger-firmware/datalogger.py
@@ -1,34 +1,35 @@
 import pyb
 from util import *
+import utime
 
-DELAY_BETWEEN_CYCLES = 5000
+#how delay between data captures in milliseconds
+DELAY_BETWEEN_CYCLES = 1000
+#frequency of data capture in a frame in Hz
+FREQ = 25
+#capture frame duration in milliseconds
+DURATION = 900
+_in_frame_delay = 1000 // FREQ
+_n = DURATION // _in_frame_delay
 accel = pyb.Accel()
 switch = pyb.Switch()
 rtc = pyb.RTC()
 rtc.wakeup(DELAY_BETWEEN_CYCLES)
+datetime = rtc.datetime()
+filename = "/sd/log.{}-{:0>2}-{:0>2}.csv".format(datetime[0], datetime[1], datetime[2])
 
-
-
-def log_series(delay = 20, duration = 5000):
-    with open('/sd/log.csv', 'a+') as log:
-        n = duration // delay
-        t = pyb.millis()
+def log_series():
+    with open(filename, 'a+') as log:
+        t = utime.time()
         x, y, z = accel.filtered_xyz()
         log.write('{},{},{},{}\n'.format(t, x, y, z))
-        for i in range(2, n+1):
+        for i in range(1, _n):
             x, y, z = accel.filtered_xyz()
             log.write(',{},{},{}\n'.format(x, y, z))
 
-# def log_file_header():
-#     with open('/sd/log.csv', 'a+') as log:
-#         log.write('Time,X,Y,Z\n')
-
-# log_file_header()
-
 while True:
     log_series()
-    blink_led(red)
+    blink_led(orange, 2, 20)
     if switch():
         blink_led(blue)
         break
-    pyb.standby()
+    pyb.stop()
