562ad0f91172beaec5c99044e8b01f02710aa124
Max Kviatkouski <kvyatkovskij@gmail.com>
2018-07-24 06:45:29 -0700

    managed to visualize using seaborn

--- log-processing/visualization/simple-seaborn-chart.py
+++ log-processing/visualization/simple-seaborn-chart.py
@@ -2,6 +2,9 @@ import seaborn as sns
 import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use('TkAgg')
 
 def data_parser(dstring):
     s = dstring[0]
@@ -9,11 +12,14 @@ def data_parser(dstring):
     return d
 
 def main():
-    dframe = pd.read_csv('../../../pyboard-current-meter-logger-data/log.fixed-time.2018-07-20.csv',
-                         header=None, index_col=0)
+    series = pd.read_csv('../../../pyboard-current-meter-logger-data/log.2018-07-20.fixed-time.csv',
+                         header=None, index_col=0, squeeze=True, parse_dates=True)
+    series.resample('5m').mean()
+    df = series.to_frame()
+    df['rolling_mean'] = series.rolling(window=15).mean()
     sns.set_style(style="whitegrid")
     print("Loaded Data Frame")
-    sns.relplot(data=dframe, palette="tab10")
+    sns.lineplot(hue='region', style='event', data=df, size=(1600, 900))
     print("Plotted")
     plt.show()
     print("Success")
