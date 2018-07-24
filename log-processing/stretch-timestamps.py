236f7622d588f9a726a26bfa44d1e3053d2aa418
Max Kviatkouski <kvyatkovskij@gmail.com>
2018-07-23 22:55:40 -0700

    implemented timestamp correction

--- log-processing/stretch-timestamps.py
+++ log-processing/stretch-timestamps.py
@@ -1,16 +1,44 @@
 #!/usr/bin/env python3
+import sys
+from datetime import datetime, timedelta, timezone
+import time
 
 def load_corrections(filename):
-    date_fomat = '(%Y, %-m, %-d, %-H, %-M, %-S)'
+    df = '(%Y, %m, %d, %H, %M, %S)'
+    def get_date_string(line):
+        return line.split(':')[-1].strip()
     with open(filename) as f:
         lines = f.readlines()
-        start_ = lines[0]
-        end_actual_ = lines[-2]
-        end_expected_ = lines[-1]
-        pass
+        start_utc = datetime.strptime(get_date_string(lines[0]), df)
+        end_actual_utc = datetime.strptime(get_date_string(lines[-2]), df)
+        end_expected_utc = datetime.strptime(get_date_string(lines[-1]), df)
+        td_actual = end_actual_utc - start_utc
+        td_expected = end_expected_utc - start_utc
+        k = td_expected.total_seconds()  / td_actual.total_seconds()
+        offset = time.localtime().tm_gmtoff // 3600
+        start_local = start_utc + timedelta(hours=offset)
+        return start_local, k
+
+def correct(timestamp, start_, k):
+    td = timestamp - start_
+    correct_td = timedelta(seconds=round((td.total_seconds() * k)))
+    correction = correct_td - td
+    print("correction={}".format(correction))
+    return timestamp + correction
 
 def main():
-    pass
+    CSV_DF = '%Y-%m-%d %H:%M:%S'
+    start, k = load_corrections(sys.argv[1])
+    print("K = {}".format(k))
+    print("Sync time = {}".format(start))
+    f = open('../../pyboard-current-meter-logger-data/log.2018-07-20.processed.csv', 'r')
+    for line in f.readlines():
+    # for line in sys.stdin:
+        t_ = datetime.strptime(line.split(',')[0], CSV_DF)
+        t = correct(t_, start, k)
+        vals = line.split(',')
+        vals[0] = t.strftime(CSV_DF)
+        print(','.join(vals), end='')
 
 if __name__ == "__main__":
     main()
