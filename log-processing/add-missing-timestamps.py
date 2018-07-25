52ae5882d254f7831265518250dfe9e4068a5684
Max Kviatkouski <kvyatkovskij@gmail.com>
2018-07-24 20:51:58 -0700

    - created empty script to implement structural refinement of datalog

---
+++ log-processing/add-missing-timestamps.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#this script will simply repeat same timestamp for all records in a measurement frame to make csv processing easier
+import sys
+
+
+def main():
+    for line in sys.sdin:
+        pass
+
+if __name__ == "__main__":
+    main()
