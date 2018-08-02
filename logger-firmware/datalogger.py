534658c68ebcca842def52478bd39eecb015306b
Max Kviatkouski <kvyatkovskij@gmail.com>
2018-08-01 23:38:15 -0700

    switched to continous mode to allow sampling

--- logger-firmware/datalogger.py
+++ logger-firmware/datalogger.py
@@ -7,7 +7,7 @@ DELAY_BETWEEN_CYCLES = 15 * 1000
 #frequency of data capture in a frame in Hz
 FREQ = 10
 #capture frame duration in milliseconds
-DURATION = 2000
+DURATION = 1000
 _in_frame_delay = 1000 // FREQ
 _n = DURATION // _in_frame_delay
 accel = pyb.Accel()
@@ -19,10 +19,12 @@ filename = "/sd/log.{}-{:0>2}-{:0>2}.csv".format(datetime[0], datetime[1], datet
 
 def log_series():
     with open(filename, 'a+') as log:
-        t = utime.time()
-        x, y, z = accel.filtered_xyz()
         for i in range(0, _n):
+            t = utime.time()
+            x, y, z = accel.filtered_xyz()
             log.write('{},{},{},{}\n'.format(t, x, y, z))
+            # blink_led(orange, 2, 20)
+            pyb.delay(_in_frame_delay)
         # for i in range(1, _n):
         #     x, y, z = accel.filtered_xyz()
         #     log.write(',{},{},{}\n'.format(x, y, z))
@@ -32,4 +34,5 @@ while True:
     if switch():
         blink_led(blue)
         break
+    pyb.delay(500)
     # pyb.stop()
