6d55d4dfa6929159dde8d7ac2cecddc27483307a
Max Kviatkouski <kvyatkovskij@gmail.com>
2018-08-12 16:04:26 -0700

    added exception handling to the script to make troubleshooting easier, fixed pyboard script error where tuple could not be converted to a string

--- log-processing/setrtc.py
+++ log-processing/setrtc.py
@@ -10,17 +10,23 @@ def main():
     pyb.enter_raw_repl()
     now = datetime.utcnow()
     print('Setting UTC time')
-    pyb.exec_raw(
+    pyb.exec_raw(""
+        "import sys\n"
         "rtc = pyb.RTC()\n"
         "t_py = ({0},{1},{2},{3},{4},{5},{6},{7})\n"
-        "t = ({0}, {1}, {2}, {4}, {5}, {6})\n"
+        "t = '({0}, {1}, {2}, {4}, {5}, {6})'\n"
         "rtc.datetime(t_py)\n"
         "print('UTC Time now is:')\n"
         "print(rtc.datetime())\n"
-        "with open('datetime.correction', 'w+') as f:\n"
-        "    f.write('Initialized UTC RTC with:' + t + '\\n')\n"
-        "    f.flush()\n"
-        "    f.close()\n"
+        "try:\n"
+        "   with open('datetime.correction', 'a+') as f:\n"
+        "       f.write('Initialized UTC RTC with:' + t + '\\n')\n"
+        "       f.flush()\n"
+        "       f.close()\n"
+        "       print('Done writing to file')\n"
+        "except Exception as exc:\n"
+        "   sys.print_exception(exc)\n"
+        "print('PyBoard execution finished.')\n"
         "".format(now.year, now.month, now.day, now.date().weekday() + 1, now.hour, now.minute, now.second + 1, 0),
         data_consumer=write_to_console
     )
