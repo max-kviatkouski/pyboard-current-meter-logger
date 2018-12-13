4ee8a549bb73e96ffe9d0fa43624a4ce4476a24e
Max Kviatkouski <kvyatkovskij@gmail.com>
2018-12-12 23:13:27 -0800

    make booter switch board into a deep sleep by default that will allow to set clock long before the deployment and all I need to do before deployment is to reset board and hold USR button only first time to enter logging mode (3 orange blinks)

--- logger-firmware/boot.py
+++ logger-firmware/boot.py
@@ -1,18 +1,25 @@
 import pyb
 from util import *
 
-# pyb.usb_mode('CDC+HID')
-
 orange.on()
 pyb.delay(1000)
-switch_value = pyb.Switch()()
+switch = pyb.Switch()
+switch_value = switch.value()
 orange.off()
 
 if switch_value:
-	pyb.usb_mode('CDC+MSC')
-	blink_led(green)
-	pyb.main('cardreader.py')
+	blue.on()
+	pyb.delay(1000)
+	switch_value = switch.value()
+	blue.off()
+	if switch_value:
+		pyb.usb_mode('CDC+MSC')
+		blink_led(green)
+		pyb.main('cardreader.py')
+	else:
+		pyb.usb_mode('CDC+HID')
+		blink_led(orange)
+		pyb.main('datalogger.py')
 else:
-	pyb.usb_mode('CDC+HID')
 	blink_led(red)
-	pyb.main('datalogger.py')
+	pyb.standby()
